2d4a248e89462199243f92a4c07939c39998b45c
mahitha243 <mahithadasari24@gmail.com>
2022-04-04 21:09:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,200 @@
+<template>
+  <div id="app">
+
+
+    <section id="search-bar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <h2>AfterSchoolApp</h2>
+                    </div>
+                    <div class="col-md-7">
+                        
+                            <input class="form-control" type="search" placeholder="Search" aria-label="Search" v-on:keyup="searchLessons" v-model="searchTxt">
+                       
+                    </div>
+                    <div class="col-md-2 cart">
+                        <button class="btn btn-1 btn-info" v-bind:disabled="(cartItems.length <= 0)"
+                        v-on:click="updateShowProduct"><p class="fa fa-shopping-cart mt-2" style="color: white;"></p> <span
+                            style="color: white">{{cartCount()}}</span></button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="navigation">
+            <div class="container">
+                <div class="row">
+                    <div style="padding-left: 6rem">
+                        <p>Home / Browse Lessons</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section v-if="showProduct" id="product">
+            <Lessons :lessons="lessons" v-on:addLesson="addLessonToCart"></Lessons>
+        </section>
+
+        <section v-else id="check-out">
+            <Checkout :cartItems="cartItems" v-on:removeLesson="removeLessonFromCart" v-on:checkOut="checkOut"></Checkout>
+            
+        </section>
+  </div>
+</template>
+
+<script>
+import Lessons from './components/Lessons.vue'
+import Checkout from './components/Checkout.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Lessons,
+    Checkout
+  },
+  data() {
+    return {
+
+       lessons: [],
+      
+        cartItems: [
+
+        ],
+        showProduct: true,
+        searchTxt: '',
+        
+        
+    }
+  },
+  methods: {
+    addLessonToCart: function (lessonID, lesson) {
+          
+            const lessonIndex = this.lessons.findIndex(lesson => lesson._id === lessonID);
+            if (lessonIndex != -1) {
+                this.lessons[lessonIndex].availablespace -= 1;       
+            }
+             this.addToCart(lessonID, lesson);
+        },
+        
+        updateShowProduct: function () {
+            this.showProduct = !this.showProduct;
+        },
+        addToCart: function (lessonID, lesson) {
+          console.log("here");
+            const itemIndex = this.cartItems.findIndex(item => item.lessonID === lessonID);
+            if (itemIndex != -1) {
+                this.cartItems[itemIndex].space += 1;
+            } else {
+                this.cartItems.push({ lessonID: lessonID, space: 1, lesson: lesson });
+            }
+        },
+        removeLessonFromCart: function (index) {
+            this.cartItems[index].space = this.cartItems[index].space - 1;
+            const lessonIndex = this.lessons.findIndex(lesson => lesson._id === this.cartItems[index].lessonID);
+            if (lessonIndex != -1) {
+                this.lessons[lessonIndex].availablespace += 1;
+            }
+            if (this.cartItems[index].space == 0) {
+                this.cartItems.splice(index, 1);
+            }
+
+            if(this.cartItems.length == 0){
+              this.showProduct = true;
+            }
+
+            console.log(this.cartItems);
+
+
+        },
+        cartCount: function(){
+            let cartQuantity = 0;
+            for (let index = 0; index < this.cartItems.length; index++) {
+                cartQuantity += this.cartItems[index].space;
+                
+            }
+            return cartQuantity;
+        },
+        searchLessons: function () {
+            
+            fetch("https://cw2-backend.herokuapp.com/collection/lesson/" + this.searchTxt).then(
+            (response) => {
+                response.json().then(
+                    (data) => {
+                        console.log(data);
+                        this.lessons = data;
+                        console.log(this.lessons);
+                    });
+            })
+        },
+        checkOut: function (checkOutName, checkOutNumber) {
+            fetch('https://cw2-backend.herokuapp.com/collection/order', {
+                method: 'POST', // set the HTTP method as 'POST'
+                headers: {
+                    'Content-Type': 'application/json', // set the data type as JSON
+                },
+                body: JSON.stringify({ name: checkOutName, phone: checkOutNumber, lessons: this.cartItems }), // need to stringify the JSON object
+            })
+                .then(response => response.json())
+                .then(responseJSON => {
+                    console.log('Success:', responseJSON);
+                });
+
+            for (let index = 0; index < this.cartItems.length; index++) {
+                const lesson = this.cartItems[index].lesson;
+                fetch(`https://cw2-backend.herokuapp.com/collection/lesson/${lesson._id}`, {
+                    method: 'PUT', // set the HTTP method as 'POST'
+                    headers: {
+                        'Content-Type': 'application/json', // set the data type as JSON
+                    },
+                    body: JSON.stringify({ availablespace: lesson.availablespace}), // need to stringify the JSON object
+                })
+                    .then(response => response.json())
+                    .then(responseJSON => {
+                        console.log('Success:', responseJSON);
+                    });
+            }
+            // alert("Check out successful\nYour order has been submitted");
+            this.showProduct = true;
+            this.cartItems = [];
+            this.$swal({
+                title: "Check out successful",
+                text: "Your order has been submitted",
+                icon: "success",
+            });
+            
+        },
+        fetchLesson: function (_id) {
+            const lessonIndex = this.lessons.findIndex(lesson => lesson._id === _id);
+            if (lessonIndex == -1) {
+                return;
+            }
+            return this.lessons[lessonIndex];
+        }
+       
+    },
+    
+    created: function () {
+        fetch("https://cw2-backend.herokuapp.com/collection/lesson/").then(
+             (response) => {
+                response.json().then(
+                     (data) => {
+                        console.log(data);
+                        this.lessons = data;
+                        console.log(this.lessons + "Fetched");
+                    });
+            })
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Lessons.vue
@@ -0,0 +1,166 @@
+<template>
+
+      <div class="container">
+                <div class="row">
+                    <div class="col-md-2">
+                        <div class="dropdown show drop-down-1">
+                            <p><strong>Sort by</strong></p>
+                            <select class="btn select-btn-1 btn-info dropdown-toggle" style="width: 100%"  v-model="attribute">
+                                <option v-for="(sortAttribute, key) in sortAttributes" :key="sortAttribute" v-bind:value="sortAttribute">{{ key }}</option>
+                            </select>
+
+                        </div>
+
+                        <div class="dropdown show drop-down-2">
+                            <p><strong>Order by</strong></p>
+                            <select class="btn select-btn-2 btn-info dropdown-toggle" style="width: 100%" v-model="order">
+                                <option v-for="(sortOrder, key) in sortOrders" :key="sortOrder" v-bind:value="sortOrder">{{ key }}</option>
+                            </select>
+                        </div>
+                    </div>
+                   <div class="col-md-10">
+                      
+                        <div v-if="(lessons.length > 0)" class="row grid-1">
+
+                            
+                            <div v-for="(lesson) in sortedLessons" :key="lesson.id" class="col-md-4" style="margin-bottom: 20px;">
+                                <div class="col-md-12 subject-1">
+                                    <div class="row">
+                                        <div class="card-img">
+                                            <img v-bind:src="`https://cw2-backend.herokuapp.com/${lesson.image}`" alt=""
+                                                style="width: 100px; height: 100px; margin-top: 20px">
+                                        </div>
+                                        <div class="card-body subject-box">
+                                            <P>Subject: {{lesson.subject}}<br> Location: {{lesson.location}}<br> Price: <span>&#163;{{lesson.price}}</span><br>
+                                                Spaces: {{lesson.availablespace}} <br>  Rating: <span v-bind:class="lesson.icon">★★★★★</span></P>
+                                                
+                                               
+                                        </div>
+                                        
+                                    </div>
+                                    <button v-bind:disabled="(lesson.availablespace <= 0)" v-on:click="addLesson(lesson._id, lesson)" type="button" class="btn btn-1 btn-info" style="width: 100%">Add to
+                                        Cart</button>
+                                </div>
+                            </div>
+                            
+
+                           
+
+                        </div>
+                        <div v-else class="row grid-1">
+                            <p style="margin: auto;">No result found</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+    
+</template>
+
+<script>
+
+export default {
+    name: 'Lessons',
+    props: ['lessons'],
+    methods: {
+        addLesson: function (lessonID, lesson) {
+             this.$emit('addLesson', lessonID, lesson);
+        },
+    },
+    data() {
+        return {
+           
+        order: 'asc',
+        attribute: 'subject',
+        sortOrders: {
+            Ascending: 'asc',
+            Descending: 'desc'
+        },
+        sortAttributes: {
+            Subject: 'subject',
+            Location: 'location',
+            Price: 'price',
+            Spaces: 'spaces'
+        },
+        }
+    },
+    computed: {
+        sortedLessons() {
+            function compareSubjectAsc(a, b) {
+                if (a.subject > b.subject) return 1;
+                if (a.subject < b.subject) return -1;
+                return 0;
+            }
+            function compareSubjectDesc(a, b) {
+                if (a.subject > b.subject) return -1;
+                if (a.subject < b.subject) return 1;
+                return 0;
+            }
+
+            function compareLocationAsc(a, b) {
+                if (a.location > b.location) return 1;
+                if (a.location < b.location) return -1;
+                return 0;
+            }
+            function compareLocationDesc(a, b) {
+                if (a.location > b.location) return -1;
+                if (a.location < b.location) return 1;
+                return 0;
+            }
+
+            function comparePriceAsc(a, b) {
+                if (a.price > b.price) return 1;
+                if (a.price < b.price) return -1;
+                return 0;
+            }
+            function comparePriceDesc(a, b) {
+                if (a.price > b.price) return -1;
+                if (a.price < b.price) return 1;
+                return 0;
+            }
+
+            function compareSpacesAsc(a, b) {
+                if (a.space > b.space) return 1;
+                if (a.space < b.space) return -1;
+                return 0;
+            }
+            function compareSpacesDesc(a, b) {
+                if (a.space > b.space) return -1;
+                if (a.space < b.space) return 1;
+                return 0;
+            }
+
+            if (this.order == "asc") {
+                if (this.attribute == "subject") {
+                  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+                    return this.lessons.sort(compareSubjectAsc);
+                } else if (this.attribute == "location") {
+                  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+                    return this.lessons.sort(compareLocationAsc);
+                } else if (this.attribute == "price") {
+                  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+                    return this.lessons.sort(comparePriceAsc);
+                } else {
+                  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+                    return this.lessons.sort(compareSpacesAsc);
+                }
+
+            } else {
+                if (this.attribute == "subject") {
+                  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+                    return this.lessons.sort(compareSubjectDesc);
+                } else if (this.attribute == "location") {
+                  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+                    return this.lessons.sort(compareLocationDesc);
+                } else if (this.attribute == "price") {
+                  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+                    return this.lessons.sort(comparePriceDesc);
+                } else {
+                  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+                    return this.lessons.sort(compareSpacesDesc);
+                }
+            }
+        }
+    },
+}
+</script>
